8aa8d9e5ed7b6e4dd164278f4396179171407034
Abesh jha <magicianabesh123@gmail.com>
2022-11-24 00:19:13 +0530

    redirect

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 
-function submit_form(){  
-alert("Login successfully");  
-}  
-
-function create(id,pass){  
-    location.href="https://ica.edu.np/Dems/API/project_api.php?email="+ id + "&password="+pass
-}  
-
 //<!-- Sending value to Android -->
 function showAndroidToast(id,pass) {
     AndroidInterface.showToast(id,pass);
 }
-
+function showDirect(id,pass) {
+    window.location.href = "https://ica.edu.np/Dems/API/project_api.php?email="+ id + "&password="+pass; 
+}
 // <!-- Getting value from Android -->
 function showVersion(msg) {
     var myVar = AndroidInterface.getAndroidVersion();
