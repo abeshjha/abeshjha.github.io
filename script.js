1b21881ac9c9e47c1716f0515a191b4c371cff73
Abesh jha <magicianabesh123@gmail.com>
2022-11-24 01:35:53 +0530

    redirection added

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 function showAndroidToast(id,pass) {
     AndroidInterface.showToast(id,pass);
 }
-function showDirect(id,pass) {
+function showDirect() {
+    var id = document.getElementById("username").value;
+    var pass = document.getElementById("pass").value;
+
     window.location.href = "https://ica.edu.np/Dems/API/project_api.php?email="+ id + "&password="+pass; 
 }
 // <!-- Getting value from Android -->
